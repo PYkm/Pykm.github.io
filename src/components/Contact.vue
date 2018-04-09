1d79ef627e05c0eaf95973a5e4186a38a0c2f763
PYkm <tingbao817@163.com>
2018-04-09 21:11:00 +0800

    First vue commit for the website

---
+++ src/components/Contact.vue
@@ -0,0 +1,52 @@
+<template>
+	<div id="Contact">
+		<div class="content">
+		<p>Hi, happy to see you in this page, if you want to communicate with me on something we both interested, you may send me an <a target="_blank" title="send me email" href="mailto:jennifer.chan3@hotmail.com">email</a>. </p>
+		<p>You may <a target="_blank" title="follow me at Zcool" href="http://www.zcool.com.cn/u/2275669">follow me at Zcool</a>, Sometime I may upload some of my graffiti.</p>
+		<p>I'm a Github User too. Here is my <a target="_blank" title="follow me at Github" href="https://github.com/pykm">personal page</a>.</p>
+
+		<p>And My <a target="_blank" title="" href="https://pykm.github.io/Blog/">Blog</a> will be updated from time to time.</p>
+  		</div>
+	</div>
+	
+</template>
+
+
+
+<script>
+	
+export default {
+  name: 'Contact',
+  data () {
+    return {
+
+    }
+  }
+}
+	
+	
+</script>
+
+
+<style scoped>
+	
+.content{
+	padding: 10%;
+}
+
+.content p{
+	font-size:1.3em;
+	text-align: left;
+	margin-top: 2%;
+}
+
+.content a{
+	color: #bbaaaa;
+    text-decoration: none;
+}
+
+.content a:hover, .content a:visited{
+	color: #ccaaaa;
+    text-decoration: none;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,131 @@
+<template>
+	<div class="header">
+
+	<div class="menu-list">
+    <!--<a class="menu_button" @click="toggleMenuList"></a>-->
+    <a class="logo" href="/_"></a>
+
+    
+  <!--<transition name="fade">
+    <div class="list" v-show="list">-->
+  <div class="list">
+    <ul>
+      <li v-for="(nav,index) in navs" :key="index">
+        <router-link tag="a" :to="'/' + nav.name">{{nav.name}}</router-link>  
+      </li>
+    </ul>
+  </div>
+  <!--</div>
+  </transition>-->
+  
+	</div>
+
+	<router-view></router-view>
+
+	</div>
+
+</template>
+
+
+<script>
+
+export default {
+  name: 'Header',
+  list: false,
+  data () {
+    return {
+    navs: [
+      {name: 'Home'},
+      {name: 'About'},
+      {name: 'Contact'}
+      /*,
+      {name: 'Contact'}*/
+    ],
+    list: this.list,
+    activeName: this.$route.path.slice(1,),
+    }
+  },
+  methods: {
+    toggleMenuList: function(){
+      this.list = !this.list;      
+    }
+  }
+}
+
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header{
+  height:90%;
+}
+
+
+.menu-list{
+  width:100%;
+  height:10%;
+  min-height:58px;
+}
+
+.logo{
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    background: url(../assets/logo.png) center center no-repeat;
+    background-size: 50px;
+    top: 8px;
+    left: 12px;
+}
+
+.menu_button{
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    background: url(../assets/menu.png) center center no-repeat;
+    background-size: 50px;
+    top: 5px;
+    right: 5%;
+    margin-left: -15px;
+
+}
+
+ul {
+  list-style-type: none;
+  padding: 10px;
+  width:90%;
+  float:right;
+}
+li {
+  display: inline-block;
+  margin: 10px 10px;
+}
+a {
+  font-size:1em;
+  font-weight:600;
+  color: #ffffee;
+  text-transform: uppercase;
+  text-decoration: none;
+}
+
+ul li a:hover{
+  color: #668855;
+}
+
+.router-link-active{
+  color: #2c3e50;
+
+}
+
+#Home, #About, #Contact{
+  margin:5% auto 0;
+  /*height:90%;*/
+  min-height: 532px;
+  background: #ffffee;
+  width:90%;    
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
